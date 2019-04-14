65df04db516a7231419fe324338bbfe99f1c05ce
gaohang <isaackao@foxmail.com>
2019-04-14 16:54:26 +0800

    引入图标，使用parcel编译sass

--- src/button.vue
+++ src/button.vue
@@ -1,35 +1,40 @@
 <template lang="html">
-    <button class="c-button">默认按钮</button>
+    <button class="c-button">
+      <svg class="icon">
+        <use xlink:href="#isetting"></use>
+      </svg>
+      默认按钮
+    </button>
 </template>
 
 <script>
+    import '../common.scss';
     export default {
 
     }
 </script>
 
 <style lang="scss" scoped>
-    @import '../common.scss'
 
-    .c-button {
-        outline: none;
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+  .c-button {
+    outline: none;
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
 
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
-        }
+    &:hover {
+      border-color: var(--border-color-hover);
     }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
 </style>
