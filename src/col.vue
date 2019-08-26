cf87c7e1b489fdcc3e17e4c3ac0de47e73b0b3d3
gaohang <isaackao@foxmail.com>
2019-08-26 09:03:26 +0800

    完成 row col 初版

--- src/col.vue
+++ src/col.vue
@@ -1,30 +1,52 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
-    </div>
+	<div
+		class="col"
+		:class="[span && `${span} col-${span}`, offset && `offset-${offset}`]"
+		:style="{ paddingLeft: `${gutter / 2}px`, paddingRight: `${gutter / 2}px` }"
+	>
+		<div style="height: 100px;">
+			<slot></slot>
+		</div>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: 'wheel-column',
-    props: {
-      span: {
-        type: [Number, String]
-      }
-    },
-  }
+export default {
+	name: "wheel-column",
+	props: {
+		span: {
+			type: [Number, String]
+		},
+		offset: {
+			type: [Number, String]
+		}
+	},
+	data() {
+		return {
+			gutter: 0
+		};
+	}
+};
 </script>
 
 <style scoped lang="scss">
-    .col {
-        height: 100px;
+.col {
+	height: 100px;
 
-        /*生成24个 .col-24 类*/
-        $class-prefix: col-;
-        @for $n from 1 through 24 {
-            &.#{$class-prefix}#{$n} {
-                width: ($n / 24) * 100%;
-            }
-        }
-    }
+	/*生成24个 .col-24 类*/
+	$class-prefix: col-;
+	@for $n from 1 through 24 {
+		&.#{$class-prefix}#{$n} {
+			width: ($n / 24) * 100%;
+		}
+	}
+
+	/*生成24个 .offset-24 类*/
+	$class-prefix: offset-;
+	@for $j from 1 through 24 {
+		&.#{$class-prefix}#{$j} {
+			margin-left: ($j / 24) * 100%;
+		}
+	}
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,25 +1,47 @@
 <template>
-    <div class="row">
-        <slot></slot>
-    </div>
+	<div
+		class="row"
+		:style="{ marginLeft: `-${gutter / 2}px`, marginRight: `-${gutter / 2}px` }"
+	>
+		<slot></slot>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: 'row'
-  }
+export default {
+	name: "wheel-row",
+	props: {
+		gutter: {
+			type: [Number, String]
+		}
+	},
+	data() {
+		return {};
+	},
+	mounted() {
+		this.$children.forEach(vm => {
+			vm.gutter = this.gutter;
+		});
+	}
+};
 </script>
 
 <style scoped lang="scss">
-    .row {
-        display: flex;
+.row {
+	display: flex;
+	/*background-color: #F6F6F6;*/
+	/*background-clip: padding-box;*/
+	/*padding-top: 10px;*/
+	/*padding-bottom: 10px;*/
 
-        > div:nth-child(odd) {
-            background-color: #D8D827;
-        }
+	> div:nth-child(odd) {
+		background-color: #d6e4aa;
+		background-clip: content-box;
+	}
 
-        > div:nth-child(even) {
-            background-color: #EBEB14;
-        }
-    }
+	> div:nth-child(even) {
+		background-color: #83b582;
+		background-clip: content-box;
+	}
+}
 </style>
